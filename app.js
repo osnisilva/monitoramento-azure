bc804f78f79ace7032bb1cab11a01f0a73d473fc
Osni Silva <osniaugusto@gmail.com>
2026-05-26 16:17:55 -0300

    Corrigindo porta Azure

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const http = require('http');
 
+const port = process.env.PORT || 3000;
+
 const server = http.createServer((req, res) => {
     console.log("Nova requisição recebida");
     res.end("Aplicação monitorada");
 });
 
-server.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
+server.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
 });
